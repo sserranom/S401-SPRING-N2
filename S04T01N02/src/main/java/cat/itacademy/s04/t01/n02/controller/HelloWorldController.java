a8fb19375b66c3e7d8f59879dacf694c11f27ede
sserranom <sergiomelgara@gmail.com>
2025-04-22 17:13:05 +0200

    Se agreda @RestController y  @RequestMapping

--- S04T01N02/src/main/java/cat/itacademy/s04/t01/n02/controller/HelloWorldController.java
+++ S04T01N02/src/main/java/cat/itacademy/s04/t01/n02/controller/HelloWorldController.java
@@ -1,20 +1,19 @@
 package cat.itacademy.s04.t01.n02.controller;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
+@RestController
+@RequestMapping("/api")
 public class HelloWorldController {
 
 
     @GetMapping("/HelloWorld")
-    public String saluda(@RequestParam(defaultValue = "UNKNOWN") String name) {
+    public String sayHello(@RequestParam(defaultValue = "UNKNOWN") String name) {
         return "Hola, " + name + ". Estás ejecutando un proyecto Gradle";
     }
 
 
     @GetMapping("/HelloWorld2/{name}")
-    public String saluda2(@PathVariable(required = false) String name) {
+    public String sayHello2(@PathVariable(required = false) String name) {
         if (name == null) {
             return "Hola, Estás ejecutando un proyecto Gradle";
         }
